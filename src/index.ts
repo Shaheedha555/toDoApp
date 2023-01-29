05c4c1601fb35f4dcb509f3d361fc480c2196dd3
Shaheedha <shaheedhamolshahi@gmail.com>
2023-01-29 14:05:28 +0530

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,75 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import {v4 as uuidV4} from 'uuid'
+// console.log(uuidV4());
+type Item = {
+  id : string,
+  title : string,
+  isComplete : boolean,
+  createdAt : Date
+}
+const list = document.getElementById('list') as HTMLUListElement | null
+const input = document.getElementById('newItem-title') as HTMLInputElement | null
+const form = document.getElementById('newItem-form') as HTMLFormElement | null
 
-import confetti from 'canvas-confetti';
+let List : Item[] = loadTasks()
+List.forEach(addToList)
+form?.addEventListener('submit', e => {
+  e.preventDefault()
+     
+  if(input?.value == '' || input?.value == null) return
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+  const newItem : Item = {
+    id : uuidV4(),
+    title : input?.value,
+    isComplete : false,
+    createdAt : new Date()
+  }
+  List.push(newItem)
+  addToList(newItem)
+  saveItems()
+  input.value = ""
+})
+
+function addToList(item : Item) : boolean{
+  const listItem = document.createElement('li')
+  listItem.setAttribute('style','margin:0.5rem;')
+  listItem.setAttribute('id',item.id)
+  const label = document.createElement('label')
+  const checkbox = document.createElement('input')
+  const itemBox = document.createElement('div')
+  const deleteButton = document.createElement('button')
+  deleteButton.append('delete')
+  deleteButton.setAttribute('id',item.id)
+  deleteButton.addEventListener('click',(e : Event)=>{
+      let button = e.target as HTMLButtonElement
+      let id =  button.id
+      
+    List = List.filter((i)=> i.id !== id)
+    let li = document.getElementById(id)
+    if(li){
+      li.remove()
+    }    
+    saveItems()
+  })
+  checkbox.addEventListener('change', ()=>{
+    item.isComplete = checkbox.checked
+    saveItems()
+  })
+  checkbox.type = 'checkbox'
+  checkbox.checked = item.isComplete
+  label.append(checkbox,item.title)
+  itemBox.append(label,deleteButton)
+  itemBox.setAttribute('style','display:flex; flex-direction: row; justify-content: space-between;')
+  listItem.append(itemBox)
+  list?.append(listItem)
+  return true
+}
+
+function saveItems(){
+  localStorage.setItem('List', JSON.stringify(List))
+}
+
+function loadTasks() : Item[]{
+const listJson = localStorage.getItem('List')
+if(listJson == null) return []
+return JSON.parse(listJson)
+}
